9cbc7d3fa2e90ea0166cbc0369802771c9e0c7f4
TamaraMojsilovic <mojsilovictamara324@gmail.com>
2022-10-30 20:27:47 +0100

    Glavni meni

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/14ebaea159.js" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="index.css">
+    <title>MedTech</title>
+</head>
+
+<body>
+
+    <div class="container">
+
+        <div class="heading-main">
+            <i class="fa-solid fa-stethoscope fa-4x"></i>
+            <h1 class="text-primary">MedTech Hospital</h2>
+        </div>
+
+        <div class="main-menu">
+            <div class="novi-pacijent">
+                <h3 id="np-h3">Novi pacijent</h3>
+            </div>
+            <div class="novi-pregled">
+                <h3 id="np-h3">Novi pregled</h3>
+            </div>
+            <div class="svi-pregledi">
+                <h3 id="np-h3">Svi pregledi</h3>
+            </div>
+        </div>
+
+
+    </div>
+
+</body>
+
+</html>
